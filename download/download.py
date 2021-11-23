6736267c081b6c3910e9dbddeb4a08c236a04533
Jenifer Tabita Ciuciu-Kiss <jenifer.girl.98@gmail.com>
2021-11-23 11:18:45 +0100

    download fix

--- download/download.py
+++ download/download.py
@@ -10,5 +10,5 @@ print(df_repos.size)
 
 for repo in df_repos:
     name ='_'.join(repo.split('/')[-2:])
-    os.system("somef describe -r {} -o tmp.json -p -t 0.8".format(name))
+    os.system("somef describe -r {} -o {}.json -p -t 0.8".format(repo, name))
     os.system("mv {}.json data/".format(name))
