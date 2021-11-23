ed09b21a8b8004528f67374b7c18c8a4202541df
Jenifer Tabita Ciuciu-Kiss <jenifer.girl.98@gmail.com>
2021-11-23 20:25:49 +0100

    fix download

--- download/download.py
+++ download/download.py
@@ -5,10 +5,12 @@ import pandas as pd
 df = pd.read_csv('../dataset/train_all.csv', sep=';')
 df_repos = df['Repo']
 df_repos.drop_duplicates(inplace=True)
-print(df_repos.size)
 #Size: 10674
 
+os.system("mkdir data")
 for repo in df_repos:
     name ='_'.join(repo.split('/')[-2:])
     os.system("somef describe -r {} -o {}.json -p -t 0.8".format(repo, name))
+    
     os.system("mv {}.json data/".format(name))
+    break
