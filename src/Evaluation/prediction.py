aa51b1eed56da344d0a197c50491fd621d34ccef
Jenifer Tabita Ciuciu-Kiss <jenifer.girl.98@gmail.com>
2023-01-14 20:51:54 +0100

    add bert to prediction

--- src/Evaluation/prediction.py
+++ src/Evaluation/prediction.py
@@ -19,7 +19,7 @@ class Predictor:
 	out_data: (Dict) Used to store the predictions.
 	out_path: (Path) Path to the output file. Defaults to {models_path}/predictions/{models_folder}.csv
 	"""
-	def __init__(self, models_path: str, data_path: str, out_path: str = None):
+	def __init__(self, models_path: str, data_path: str, out_path: str = None, bert: bool = False):
 		"""
 		Params
 		--------
@@ -29,6 +29,12 @@ class Predictor:
 		"""
 		self.models_path = Path(models_path)
 		self.data_path = data_path
+		self.bert = bert
+		if bert:
+			import tensorflow as tf
+			import tensorflow_hub as hub
+			import numpy as np
+			from bert.tokenization import bert_tokenization as tokenization
 		if out_path is not None:
 			self.out_path = Path(out_path)
 		else:
@@ -44,23 +50,68 @@ class Predictor:
 		Loads and stores all the models from the given folder.
 		"""
 		self.__models = []
+		self.model_labels = []
 		for model_file in self.models_path.iterdir():
-			with open(model_file, 'rb') as f:
-				self.__models.append(pickle.load(f))
+			if self.bert:
+				self.__models.append(tf.keras.models.load_model(model_file.absolute().as_posix(), custom_objects={'KerasLayer': hub.KerasLayer}))
+			else:
+				with open(model_file, 'rb') as f:
+					self.model_labels.append(model_file.name.split('.')[0].replace('_', ' '))
+					self.__models.append(pickle.load(f))
 
 	def saveData(self) -> None:
 		"""
 		Saves the predictions into {self.out_path} csv file with ['Labels', 'Repo', 'Predictions'] columns.
 		"""
 		with open(self.out_path, 'w') as f:
-			writer = csv.DictWriter(f, delimiter=';', fieldnames=['Labels', 'Repo', 'Predictions'])
+			writer = csv.DictWriter(f, delimiter=';', fieldnames=['Labels', 'Repo', 'Predictions', 'Probabilities'])
 			writer.writeheader()
 			for key, val in self.out_data.items():
 				writer.writerow({
 					'Labels': ','.join(val['Labels']),
 					'Repo': key,
-					'Predictions': ','.join(val['Predictions'])
+					'Predictions': ','.join(val['Predictions']),
+					'Probabilities': ','.join(val['Probabilities']),
 				})
+
+	def encoding_the_text(self, texts, tokenizer, max_len=300):
+		all_tokens = []
+		all_masks = []
+		all_segments = []
+		
+		for text in texts:
+			text = tokenizer.tokenize(text)
+			
+			text = text[:max_len-2]
+			input_sequence = ["[CLS]"] + text + ["[SEP]"]
+			pad_len = max_len-len(input_sequence)
+			
+			tokens = tokenizer.convert_tokens_to_ids(input_sequence) + [0] * pad_len
+			pad_masks = [1] * len(input_sequence) + [0] * pad_len
+			segment_ids = [0] * max_len
+			
+			all_tokens.append(tokens)
+			all_masks.append(pad_masks)
+			all_segments.append(segment_ids)
+		return np.array(all_tokens), np.array(all_masks), np.array(all_segments)
+
+	def getBertTokenizer(self):
+		m_url = 'https://tfhub.dev/tensorflow/bert_en_uncased_L-12_H-768_A-12/2'
+		bert_layer = hub.KerasLayer(m_url, trainable=True)
+		tf.gfile = tf.io.gfile
+		vocab_file = bert_layer.resolved_object.vocab_file.asset_path.numpy()
+		do_lower_case = bert_layer.resolved_object.do_lower_case.numpy()
+		return tokenization.FullTokenizer(vocab_file, do_lower_case)
+
+	def getLabelsFromPrediction(self, prediction) -> set[str]:
+		categories_alphabetical = ['Audio', 'Computer Vision', 'Graphs', 'Natural Language Processing', 'Reinforcement Learning', 'Sequential']
+		print(prediction)
+		above_thresholds_indices = np.argwhere(prediction > 0.5)
+		res = []
+		if above_thresholds_indices:
+			for ind in above_thresholds_indices:
+				res.append(categories_alphabetical[ind[0]])
+		return set(res)
 
 	def predict(self):
 		"""
@@ -71,18 +122,50 @@ class Predictor:
 		"""
 		self.__loadModels()
 		self.out_data = {}
-		with open(self.data_path) as f:
-			reader = csv.DictReader(f, delimiter=';')
-			for row in reader:
+
+
+		if self.bert:
+			tokenizer = self.getBertTokenizer()
+
+			with open(self.data_path) as f:
+				reader = csv.DictReader(f, delimiter=';')
+				text_data = [row for row in reader]
+				texts = [row['Text'] for row in text_data]
+			text = self.encoding_the_text(texts, tokenizer)
+			print(len(texts), len(text))
+			predictions = [model.predict(text) for model in self.__models]
+			for i, row in enumerate(text_data):
 				repo = row['Repo']
 				if repo in self.out_data:
 					self.out_data[repo]['Labels'].add(row['Label'])
 				else:
 					self.out_data[repo] = {'Labels' : set([row['Label']]), 'Predictions': set()}
-					for model in self.__models:
-						pred = model.predict([row['Text']])[0]
-						if pred != 'Other':
-							self.out_data[repo]['Predictions'].add(pred)
+					for prediction in predictions:
+						labels = self.getLabelsFromPrediction(prediction[i])
+						self.out_data[repo]['Probabilities'] = prediction[i]
+						print(labels)
+						self.out_data[repo]['Predictions'].update(labels)
+		else:
+			with open(self.data_path) as f:
+				reader = csv.DictReader(f, delimiter=';')
+				for row in reader:
+					repo = row['Repo']
+					if repo in self.out_data:
+						self.out_data[repo]['Labels'].add(row['Label'])
+					else:
+						self.out_data[repo] = {'Labels' : set([row['Label']]), 'Predictions': set(), 'Probabilities': []}
+						for model_ind, model in enumerate(self.__models):
+							pred = model.predict([row['Text']])
+							pred_proba = model.predict_proba([row['Text']])
+							model_label = self.model_labels[model_ind]
+							#print(f'{model_label=}')
+							#print(pred, pred_proba)
+							if 'Other' > model_label:
+								self.out_data[repo]['Probabilities'].append(str(pred_proba[0][0]))
+							else:
+								self.out_data[repo]['Probabilities'].append(str(pred_proba[0][1]))
+							if pred != 'Other':
+								self.out_data[repo]['Predictions'].add(pred[0])
 		self.saveData()
 
 if __name__ == '__main__':
@@ -90,8 +173,9 @@ if __name__ == '__main__':
 	parser_predict.add_argument('--inputfolder', required=True, help='Path of folder with the models.')
 	parser_predict.add_argument('--test_set', required=True, help='Name of the csv file containing the test set.')
 	parser_predict.add_argument('--outfile', required=True, help='Path to output csv file with the results.')
+	parser_predict.add_argument('--bert', action=argparse.BooleanOptionalAction)
 	
 	args = parser_predict.parse_args()
 
-	predictor = Predictor(args.inputfolder, args.test_set, args.outfile).predict()
+	predictor = Predictor(args.inputfolder, args.test_set, args.outfile, args.bert).predict()
 
